5403fef2fd5cbf7dc06d834724b34f76e690eb9f
id-zonex <idzone027@gmail.com>
2021-03-23 18:39:17 +0200

    Added new Camera

---
+++ TowerDefens3D/Assets/Scripts/FirstPlayerMover.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class FirstPlayerMover : MonoBehaviour
+{
+    [SerializeField] private float _speed = 4f;
+
+    [SerializeField] private float _sensivity = 5f;
+
+    private Rigidbody _rb;
+
+    private Vector3 _rotateVelocity;
+    private Vector3 _moveVelocity;
+
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    public void Move()
+    {
+        _moveVelocity.x = Input.GetAxis("Horizontal");
+        _moveVelocity.z = Input.GetAxis("Vertical");
+
+        _rb.AddRelativeForce(_moveVelocity * _speed * Time.fixedDeltaTime);
+    }
+
+    public void Rotate()
+    {
+        _rotateVelocity.x += Input.GetAxis("Mouse X");
+        _rotateVelocity.y += Input.GetAxis("Mouse Y");
+
+        _rotateVelocity.y = Mathf.Clamp(_rotateVelocity.y, -10, 10);
+
+        transform.rotation = Quaternion.Euler(-_rotateVelocity.y, _rotateVelocity.x * _sensivity * Time.fixedDeltaTime, 0f);
+    }
+}
---
+++ TowerDefens3D/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(FirstPlayerMover))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private FirstPlayerMover _firstPlayerMover;
+    [SerializeField] private TopDownCameraMover _topDownCameraMover;
+
+    [SerializeField] private Camera _firstCamera;
+    [SerializeField] private Camera _topDownCamera;
+
+    private bool IsPressed = false;
+
+
+    private void Awake()
+    {
+        _firstPlayerMover = GetComponent<FirstPlayerMover>();
+    }
+
+    private void Update()
+    {
+        TryChangeCamera();
+
+        _firstPlayerMover.Rotate();
+    }
+
+    private void FixedUpdate()
+    {
+        _firstPlayerMover.Move();
+    }
+
+    private void TryChangeCamera()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            IsPressed = !IsPressed;
+        }
+
+        if (IsPressed)
+        {
+            _firstCamera.gameObject.SetActive(!_firstCamera.enabled);
+            _topDownCamera.gameObject.SetActive(_topDownCamera.enabled);
+        }
+        else
+        {
+            _firstCamera.gameObject.SetActive(_firstCamera.enabled);
+            _topDownCamera.gameObject.SetActive(!_topDownCamera.enabled);
+        }
+    }
+}
