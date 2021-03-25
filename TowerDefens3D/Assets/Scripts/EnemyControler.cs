2df7222705e965c2881fe3b81b935993bf205821
id-zonex <idzone027@gmail.com>
2021-03-26 00:30:58 +0200

    Added tower prototype

---
+++ TowerDefens3D/Assets/Scripts/EnemyControler.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+
+public class EnemyControler : MonoBehaviour
+{
+    [SerializeField] private PathCreator _pathCreator;
+
+    [SerializeField] private float _speed;
+
+    private float _trewelledDistance;
+
+
+    private void Start()
+    {
+        _pathCreator = GameObject.Find("Path").GetComponent<PathCreator>();
+    }
+
+    private void Update()
+    {
+        _trewelledDistance += _speed * Time.deltaTime;
+
+        transform.position = _pathCreator.path.GetPointAtDistance(_trewelledDistance);
+        transform.rotation = _pathCreator.path.GetRotationAtDistance(_trewelledDistance);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        switch (other.tag)
+        {
+            case "finish":
+                Destroy(gameObject);
+                break;
+
+        }
+
+    }
+
+    public void ApplyDamage(int dmg)
+    {
+        Debug.Log($"{gameObject.name}, Ayyy");
+    }
+}
---
+++ TowerDefens3D/Assets/Scripts/Tower/Tower.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    [SerializeField] private Transform _head;
+    [SerializeField] private Transform _trunk;
+
+    private List<EnemyControler> _enemies = new List<EnemyControler>();
+
+    private void FixedUpdate()
+    {
+        if (_enemies.Count > 0)
+        {
+            EnemyControler target = _enemies[0];
+
+            float Ypos = Mathf.Clamp(target.transform.position.y, 1, 3);
+
+            _head.LookAt(new Vector3(target.transform.position.x, _head.position.y, target.transform.position.z));
+            _trunk.LookAt(new Vector3(target.transform.position.x, Ypos, target.transform.position.z));
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log($"{other.name}: Enter");
+
+        EnemyControler enemy = GetEnemy(other.gameObject);
+
+        if (enemy != null)
+            _enemies.Add(GetEnemy(other.gameObject));
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log($"{other.name}: Exit");
+
+        EnemyControler enemy = GetEnemy(other.gameObject);
+
+        if(enemy != null)
+            _enemies.Remove(GetEnemy(other.gameObject));
+    }
+
+    private EnemyControler GetEnemy(GameObject gameObject)
+    {
+        return gameObject.GetComponent<EnemyControler>();
+    }
+}
